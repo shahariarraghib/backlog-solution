4770aa0b3736e7ca33336fd1401756989ac1669a
shahariarraghib <sr.raghib@gmail.com>
2024-04-23 21:13:57 +0600

    add blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,5 @@
 import SingleBlog from "@/components/Blog/SingleBlog";
-import blogData from "@/components/Blog/blogData";
+// import blogData from "@/components/Blog/blogData";
 import Breadcrumb from "@/components/Common/Breadcrumb";
 
 import { Metadata } from "next";
@@ -22,17 +22,15 @@ const Blog = () => {
       <section className="pb-[120px] pt-[120px]">
         <div className="container">
           <div className="-mx-4 flex flex-wrap justify-center">
-            {blogData.map((blog) => (
+            {/* {blogData.map((blog) => (
               <div
                 key={blog.id}
                 className="w-full px-4 md:w-2/3 lg:w-1/2 xl:w-1/3"
               >
                 <SingleBlog blog={blog} />
               </div>
-            ))}
+            ))} */}
           </div>
-
-          
         </div>
       </section>
     </>
